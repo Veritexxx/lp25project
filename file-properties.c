026cfd77ac07e0adf8ae4b7169a54b35f1b880bf
GouNicolas <152480830+GouNicolas@users.noreply.github.com>
2023-12-10 16:44:51 +0100

    Update file-properties.c
    ex

--- file-properties.c
+++ file-properties.c
@@ -28,6 +28,25 @@
  * @return -1 in case of error, 0 else
  */
 int get_file_stats(files_list_entry_t *entry) {
+    struct stat file_info;
+    if (stat(entry->path, &file_info) == -1) {
+        perror("Error: stat");
+        return -1;
+    }
+    entry->mode = file_info.st_mode;
+    entry->size = file_info.st_size;
+    entry->mtime = file_info.st_mtime;
+    if (S_ISDIR(file_info.st_mode)) {
+        entry->type = DOSSIER;
+    } else {
+        entry->type = FICHIER;
+    }
+    if (entry->type == FICHIER) {
+        if (compute_file_md5(entry) == -1) {
+            return -1;
+        }
+    }
+    return 0;
 }
 
 /*!
@@ -37,6 +56,27 @@ int get_file_stats(files_list_entry_t *entry) {
  * Use libcrypto functions from openssl/evp.h
  */
 int compute_file_md5(files_list_entry_t *entry) {
+    unsigned char md5[MD5_DIGEST_LENGTH];
+    FILE *file = fopen(entry->path, "rb");
+    if (file == NULL) {
+        perror("Error: fopen");
+        return -1;
+    }
+    MD5_CTX md5_context;
+    MD5_Init(&md5_context);
+    unsigned char data[1024];
+    int bytes;
+    while ((bytes = fread(data, 1, 1024, file)) != 0) {
+        MD5_Update(&md5_context, data, bytes);
+    }
+    MD5_Final(md5, &md5_context);
+    fclose(file);
+    char md5_string[MD5_DIGEST_LENGTH * 2 + 1];
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        sprintf(md5_string + i * 2, "%02x", md5[i]);
+    }
+    strcpy(entry->md5sum, md5_string);
+    return 0;
 }
 
 /*!
