28db63a964e7c56e561eb321dd9c05d94574ea82
Veritexxx <152967675+Veritexxx@users.noreply.github.com>
2023-12-27 11:20:56 +0100

    Update utility.c

--- utility.c
+++ utility.c
@@ -12,17 +12,28 @@
  * @param suffix the second part of the resulting path
  * @return a pointer to the resulting path, NULL when concatenation failed
  */
-char *concat_path(char *result, char *prefix, char *suffix) {
-  
+char *concat_path(char *result, const char *prefix, const char *suffix) {
   if (result == NULL || prefix == NULL || suffix == NULL) {
     fprintf(stderr, "Erreur : Paramètres d'entrée non valides\n");
     return NULL;
   }
-  
-  if (snprintf(result, PATH_SIZE, "%s/%s", prefix, suffix) >= PATH_SIZE) {
+
+  size_t prefix_len = strlen(prefix);
+
+  // Vérifier si le préfixe se termine par "/"
+  if (prefix_len > 0 && prefix[prefix_len - 1] != '/') {
+    strcat(result, prefix);
+    strcat(result, "/");
+  } else {
+    strcat(result, prefix);
+  }
+
+  strcat(result, suffix); // Ajouter le suffixe
+
+  if (strlen(result) >= PATH_SIZE) {
     fprintf(stderr, "Erreur : Le chemin résultant dépasse la taille maximale\n");
     return NULL;
   }
-  
+
   return result;
 }
